40e2b9197926c3ada416c6ba88708134d0c23556
Your Name <you@example.com>
2021-06-01 22:56:51 -0500

    commit segundo

--- src/main/java/com/empresa/controller/MarcaController.java
+++ src/main/java/com/empresa/controller/MarcaController.java
@@ -5,8 +5,12 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,4 +45,38 @@ public class MarcaController {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@PostMapping
+	public ResponseEntity<Marca> CrearMarca(@RequestBody Marca marca){
+		Marca newMarca = marcaRepositorio.save(marca);
+		
+		return ResponseEntity.ok(newMarca);
+		
+	}
+	
+	@DeleteMapping("{id}")
+	public ResponseEntity<Void> EliminarMarca(@PathVariable("id") int id){
+		
+		marcaRepositorio.deleteById(id);
+		
+		return ResponseEntity.ok(null);
+	}
+	
+	@PutMapping
+	public ResponseEntity<Marca> editarMarca(@RequestBody Marca marca){
+		
+		Optional<Marca> optMarca = marcaRepositorio.findById(marca.getId());
+		
+		if(optMarca.isPresent()) {
+			
+			Marca updateMarca = optMarca.get();
+			updateMarca.setNombre(marca.getNombre());
+			updateMarca.setFecha(marca.getFecha());
+			marcaRepositorio.save(updateMarca);
+			return ResponseEntity.ok(updateMarca);
+		}else {
+			
+			return ResponseEntity.notFound().build();
+		}
+		
+	}
 }
