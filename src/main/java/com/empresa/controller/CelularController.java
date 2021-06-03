a578fd4c3c222c05d2048e8aeea7741d3bc9898e
JhamirCokche <jcokchellacta1@gmail.com>
2021-06-02 19:00:34 -0500

    2do Commit por Cokche Llacta

--- src/main/java/com/empresa/controller/CelularController.java
+++ src/main/java/com/empresa/controller/CelularController.java
@@ -5,8 +5,12 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,4 +39,37 @@ if(optional.isPresent()) {
 	return ResponseEntity.noContent().build();
 
 }
+
+@PostMapping
+public ResponseEntity<Celular> CrearCelular(@RequestBody Celular celular)
+{
+Celular newCelular = celularRepositorio.save(celular);
+return ResponseEntity.ok(newCelular);
+}
+
+@DeleteMapping("{id}")
+public ResponseEntity<Void> EliminarCelular(@PathVariable("id") int id_celular)
+{
+celularRepositorio.deleteById(id_celular);	
+return ResponseEntity.ok(null);
+}
+
+@PutMapping
+public ResponseEntity<Celular> editarCelular(@RequestBody Celular celular)
+{
+Optional<Celular> optCelular = celularRepositorio.findById(celular.getId_celular());	
+if(optCelular.isPresent()) {
+   Celular updateCelular = optCelular.get();
+   updateCelular.setNombre(celular.getNombre());
+   updateCelular.setPrecio(celular.getPrecio());
+   updateCelular.setMarca(celular.getMarca());
+   updateCelular.setStock(celular.getStock());
+   celularRepositorio.save(updateCelular);
+   return ResponseEntity.ok(updateCelular);
+}else {
+	return ResponseEntity.notFound().build();
+}
+}
+
+
 }
