6f407092ab34ed79cff43d5c22bfd8bebc7cd041
JhamirCokche <jcokchellacta1@gmail.com>
2021-06-22 00:44:23 -0500

    dsw

--- src/main/java/com/empresa/repository/UsuarioRepository.java
+++ src/main/java/com/empresa/repository/UsuarioRepository.java
@@ -1,10 +1,16 @@
 package com.empresa.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 
 import com.empresa.entity.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
 	public Usuario findByUserAndContraseña (String user, String contr);
+
+	public List<Usuario> findByUsuarioLike( String filtro);
+	
 }
